d28e7602aee4d4d3372efe7174d98c3a195d3028
Your Name <hardsontessi2@gmail.com>
2022-07-27 18:51:59 +0100

    slices

---
+++ list.py
@@ -0,0 +1,4 @@
+liste = [2,3,5,6]
+print(liste)
+liste.extend([7, 8, 9])
+print(liste)
---
+++ slices.py
@@ -0,0 +1,5 @@
+# Certains éléments de la liste
+# liste[0, n], n exclut
+liste = ["user01", "user02", "user03", "user04", "user05"]
+result = liste[::-1]
+print(result)
