cf09680695148e90170df45f208817db406575a4
Your Name <hardsontessi2@gmail.com>
2022-07-27 19:02:56 +0100

    sort, reverse et sorted

--- slices.py
+++ slices.py
@@ -1,5 +1,5 @@
 # Certains éléments de la liste
 # liste[0, n], n exclut
-liste = ["user01", "user02", "user03", "user04", "user05"]
-result = liste[::-1]
-print(result)
+liste = [3, 6, 5, 1, 0]
+liste.sort()
+print(liste)
