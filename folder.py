023375b31e9566fdd2ee3b7289f95205a4f61d1d
Your Name <hardsontessi2@gmail.com>
2022-07-27 18:22:03 +0100

    dir, help et pprint

--- folder.py
+++ folder.py
@@ -1,12 +1,7 @@
-import os
+from pprint import pprint
 
-chemin = './'
-dossier = os.path.join(chemin, 'dossier');
-if not os.path.exists(dossier):
-    print('Dossier introuvable')
-else:
-    print('Dossier existant');
-    response = int(input('Voulez-vous supprimer le dossier ?: '))
-    if response == 1:
-            os.removedirs(dossier);
-            print('Dossier supprimé')
+import os
+print(dir(os))
+help(os.makedirs)
+
+pprint(dir(os))
